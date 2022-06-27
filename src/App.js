d49ba32e1983858d4580119869f7e74b286279d6
nggar <enggarjpx@gmail.com>
2022-06-27 17:57:51 +0700

    style Nav component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import pages
 import HomePage from './pages/Home';
 // Style
+import Nav from './components/Nav';
 import GlobalStyle from './components/GlobalStyle';
 
 function App() {
     return (
         <div className="App">
             <GlobalStyle />
+            <Nav />
             <HomePage />
         </div>
     );
