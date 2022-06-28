2e63f7e73a327d6f1b2de07cbea8620db372a534
nggar <enggarjpx@gmail.com>
2022-06-29 00:28:08 +0700

    implement react router dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 // import pages
-import HomePage from './pages/Home';
-// Style
 import Nav from './components/Nav';
+import HomePage from './pages/Home';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
+// Style
 import GlobalStyle from './components/GlobalStyle';
+// Router
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
     return (
         <div className="App">
             <GlobalStyle />
             <Nav />
-            <HomePage />
+            <Routes>
+                <Route path="/" exact element={<HomePage />} />
+
+                <Route path="/work" element={<OurWork />} />
+
+                <Route path="/contact" element={<ContactUs />} />
+
+            </Routes>
         </div>
     );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,19 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
     return (
         <StyledNav>
-            <h1><a id="logo" href="#">Capture</a></h1>
+            <h1><Link id="logo" to="/">Capture</Link></h1>
             <ul>
                 <li>
-                    <a href="#">1. About us</a>
+                    <Link to="/">1. About us</Link>
                 </li>
                 <li>
-                    <a href="#">2. Our Work</a>
+                    <Link to="/work">2. Our Work</Link>
                 </li>
                 <li>
-                    <a href="#">1. Contact Us</a>
+                    <Link to="/contact">1. Contact Us</Link>
                 </li>
             </ul>
         </StyledNav>
