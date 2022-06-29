d7de0675b86efa56bffeacd1c22c357a27dabf99
nggar <enggarjpx@gmail.com>
2022-06-30 03:36:21 +0700

    Work animation

--- src/pages/Works.js
+++ src/pages/Works.js
@@ -6,27 +6,37 @@ import theracer from '../img/theracer-small.png';
 import goodtimes from '../img/goodtimes-small.png';
 // animation
 import { motion } from 'framer-motion';
-import { pageTransition } from '../utils/animation';
+import { pageTransition, fade, photoAnimation, lineAnimation, slideAnimation, slideContainer } from '../utils/animation';
 
 const Works = () => {
     return (
         <StyledWorks style={{ background: '#fff' }} variants={pageTransition} initial='hidden' animate='show' exit='exit'>
+
+            <motion.div variants={slideContainer}>
+                <Frame1 variants={slideAnimation}></Frame1>
+                <Frame2 variants={slideAnimation}></Frame2>
+                <Frame3 variants={slideAnimation}></Frame3>
+                <Frame4 variants={slideAnimation}></Frame4>
+            </motion.div>
+
             <StyledMovie>
-                <h2>The Athlete</h2>
-                <div className="line"></div>
+                <motion.h2 variants={fade}>The Athlete</motion.h2>
+                <motion.div variants={lineAnimation} className="line"></motion.div>
                 <Link to='the-athlete' >
-                    <img src={athlete} alt='athlete' />
+                    <Hide>
+                        <motion.img variants={photoAnimation} src={athlete} alt='athlete' />
+                    </Hide>
                 </Link>
             </StyledMovie>
             <StyledMovie>
-                <h2>The Racer</h2>
+                <motion>The Racer</motion>
                 <div className="line"></div>
                 <Link to='the-racer'>
                     <img src={theracer} alt="the racer" />
                 </Link>
             </StyledMovie>
             <StyledMovie>
-                <h2>Good Times</h2>
+                <motion>Good Times</motion>
                 <div className="line"></div>
                 <Link to='good-times'>
                     <img src={goodtimes} alt='goodtimes' />
@@ -49,7 +59,7 @@ const StyledMovie = styled.div`
     padding-bottom: 10rem;
     .line {
         height: 0.5rem;
-        background: #cccccc;
+        background: #23d997;
         margin-bottom: 3rem;
     }
     img {
@@ -58,5 +68,27 @@ const StyledMovie = styled.div`
         object-fit: cover;
     }
 `
+const Hide = styled.div`
+    overflow: hidden;
+`
 
+// Framer Animation
+const Frame1 = styled( motion.div )`
+    position: fixed;
+    left: 0;
+    top: 10%;
+    width: 100%;
+    height: 100vh;
+    background: #fffebf;
+    z-index: 2;
+`
+const Frame2 = styled( Frame1 )`
+    background: #ff8efb;
+`
+const Frame3 = styled( Frame1 )`
+    background: #8ed2ff;
+`
+const Frame4 = styled( Frame1 )`
+    background: #8effa0;
+`
 export default Works;
--- src/utils/animation.js
+++ src/utils/animation.js
@@ -4,7 +4,7 @@ export const pageTransition = {
     },
     show: {
         opacity: 1, y: 0,
-        transition: { duration: .75, when: 'beforeChildren', staggerChildren: .25 }
+        transition: { duration: .5, when: 'beforeChildren', staggerChildren: .25 }
     },
     exit: {
         opacity: 0, y: 300,
@@ -24,11 +24,26 @@ export const fade = {
     hidden: { opacity: 0 },
     show: {
         opacity: 1,
-        transition: { duration: 1 }
+        transition: { duration: .75 }
     }
 }
 
 export const photoAnimation = {
     hidden: { scale: 1.5, opacity: 0 },
     show: { scale: 1, opacity: 1, transition: { duration: .75 } }
+}
+
+export const lineAnimation = {
+    hidden: { width: '0%' },
+    show: { width: '100%', transition: { duration: 1 } }
+}
+
+export const slideAnimation = {
+    hidden: { x: '-130%', skew: '45deg' },
+    show: { x: '100%', skew: '0deg', transition: { duration: 1 } }
+}
+
+export const slideContainer = {
+    hidden: { opacity: 1 },
+    show: { opacity: 1, transition: { staggerChildren: .15, ease: 'easeOut' } }
 }
